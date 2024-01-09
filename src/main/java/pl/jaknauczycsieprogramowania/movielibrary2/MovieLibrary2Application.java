62d7d6542e4390fa0d57b4fca7ba27765044f8ff
Kornelia Syzdół <kornelia@MacBook-Pro-Kornelia.local>
2024-01-09 19:48:44 +0100

    application completed

---
+++ src/main/java/pl/jaknauczycsieprogramowania/movielibrary2/MovieLibrary2Application.java
@@ -0,0 +1,12 @@
+package pl.jaknauczycsieprogramowania.movielibrary2;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class MovieLibrary2Application {
+
+	public static void main(String[] args) {
+		SpringApplication.run(MovieLibrary2Application.class, args);
+	}
+}
